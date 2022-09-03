c0bee0a4584cd92e2582177cde081b4c5a03eddb
milind-soni <46266943+milind-soni@users.noreply.github.com>
2022-09-03 05:36:36 +0530

    Add files via upload

---
+++ source_elevate_rockset/streams.py
@@ -0,0 +1,41 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+import urllib.parse
+from abc import ABC
+from typing import Any, Iterable, Mapping, MutableMapping, Optional
+
+import requests
+from airbyte_cdk.sources.streams.http import HttpStream
+
+
+class RocksetStream(HttpStream, ABC):
+    url_base = "https://api.use1a1.rockset.com/v1/orgs/self/ws/commons/"
+
+    def __init__(self, workspace: str, **kwargs):
+        super().__init__(**kwargs)
+        self.workspace = workspace
+
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        return [response.json()]
+
+
+class Workspace(RocksetStream):
+    # Docs: https://docs.orbit.love/reference/get_workspaces-workspace-slug
+    # This stream is primarily used for connnection checking.
+    primary_key = "id"
+
+    def path(
+        self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> str:
+        return f"lambdas/{self.workspace}/tags/latest"
